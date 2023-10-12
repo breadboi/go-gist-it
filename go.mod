a723227ffae20c154e2813acf0f08db9fbe3b212
Brett Carney <brett@brettcarney.com>
2023-10-12 12:24:20 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-gist-it
+
+go 1.21
+
+require github.com/atotto/clipboard v0.1.4
---
+++ main.go
@@ -0,0 +1,27 @@
+package go_gist_it
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/atotto/clipboard"
+)
+
+func main() {
+	filePath := os.Args[1]
+
+	// Run the GitHub CLI command to create a gist
+	cmd := exec.Command("gh", "gist", "create", filePath)
+	out, _ := cmd.CombinedOutput()
+
+	// Extract the URL (assuming it's the last word in the output)
+	outputLines := strings.Split(strings.TrimSpace(string(out)), "\n")
+	gistURL := strings.TrimSpace(outputLines[len(outputLines)-1])
+
+	// Copy the gist URL to the clipboard
+	err := clipboard.WriteAll(gistURL)
+	if err != nil {
+		return
+	}
+}
